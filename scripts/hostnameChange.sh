38428f6f7dcf3dcb2733274992b042e96689829a
edwin3746 <64019173+edwin3746@users.noreply.github.com>
2022-08-03 17:14:03 +0800

    Delete hostnameChange.sh

--- scripts/hostnameChange.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~
-EXTERNAL_IP=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
-value="https://"
-value1="${value}${EXTERNAL_IP}"
-echo value1 >> myfile.txt
-
