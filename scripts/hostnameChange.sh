f2ce6c1a3558c81f54b636e8fc3f70063de0b195
edwin3746 <64019173+edwin3746@users.noreply.github.com>
2022-08-03 15:44:27 +0800

    Update hostnameChange.sh

--- scripts/hostnameChange.sh
+++ scripts/hostnameChange.sh
@@ -3,3 +3,4 @@ cd ~
 EXTERNAL_IP=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
 value="https://"
 value1="${value}${EXTERNAL_IP}"
+echo '"HOSTNAME=${value1}"' >> myenv.env
